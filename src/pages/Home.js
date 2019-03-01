90968503a55d86e0c68ad42eaf8e532be236edf9
Jordan Bryan <jbryan382@gmail.com>
2019-03-01 18:45:23 -0500

    Set up API Key, and put important info in comments of main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,12 @@
+// Important Junk
+// URL for API Usage: https://developers.themoviedb.org/3/getting-started/introduction
+// URL for API Details: https://www.themoviedb.org/settings/api
+//
+// exampleAPIRequest = 'https://api.themoviedb.org/3/movie/550?api_key=247738650e64acaaf86dc7de0021f7d6'
+// v3APIKey = '247738650e64acaaf86dc7de0021f7d6'
+// This last one is probably not needed
+// v4ReadAccessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDc3Mzg2NTBlNjRhY2FhZjg2ZGM3ZGUwMDIxZjdkNiIsInN1YiI6IjVjNzlhMzQ0MGUwYTI2MTFkYTEwMjQ5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.clN9_6vTaHIo6mu-n78B7Hlq7YvvIwczNwWSeyWB-TM'
+
 import React, { Component } from 'react'
 
 class Home extends Component {
