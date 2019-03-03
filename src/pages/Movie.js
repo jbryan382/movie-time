2fe49cd81b0dc03d2bec3f1ea8b971d199de9f76
Will Warren <willwarrenmail@gmail.com>
2019-03-03 16:53:10 -0500

    added cast, liveshare in person

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -8,7 +8,8 @@ class Movie extends Component {
     description: '',
     releaseDate: '',
     poster: '',
-    id: ''
+    id: '',
+    cast: []
   }
 
   componentDidMount() {
@@ -18,7 +19,7 @@ class Movie extends Component {
       .get(
         `https://api.themoviedb.org/3/movie/${
           this.props.match.params.movie
-        }?api_key=247738650e64acaaf86dc7de0021f7d6&append_to_response=images`
+        }?api_key=247738650e64acaaf86dc7de0021f7d6`
       )
       .then(resp => {
         console.log({ resp })
@@ -42,19 +43,33 @@ class Movie extends Component {
         }/credits?api_key=247738650e64acaaf86dc7de0021f7d6`
       )
       .then(resp => {
-        console.log(resp)
+        console.log({ resp })
+
+        this.setState({
+          cast: resp.data.cast
+        })
+        console.log(this.state.cast[1].name)
       })
   }
 
   render() {
     return (
-      <MovieDetails
-        title={this.state.title}
-        description={this.state.description}
-        genre={this.state.genre}
-        releaseDate={this.state.releaseDate}
-        poster={`https://image.tmdb.org/t/p/original${this.state.poster}`}
-      />
+      <>
+        <MovieDetails
+          title={this.state.title}
+          description={this.state.description}
+          genre={this.state.genre}
+          releaseDate={this.state.releaseDate}
+          poster={`https://image.tmdb.org/t/p/original${this.state.poster}`}
+          // cast={this.state.cast.name}
+        />
+        <ul>
+          {this.state.cast.map((cast, i) => {
+            console.log(cast)
+            return <li key={i}>{this.state.cast[i].name}</li>
+          })}
+        </ul>
+      </>
     )
   }
 }
